45ecad01ed40cd635f942acc9312ad2298a2b4ad
suneel7351 <rsuneel47@gmail.com>
2021-11-06 01:54:16 +0530

    Updates

---
+++ precache-manifest.4550a87450a926bf1eb80db6843c36db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249c4ea9c44c6db7a6bb92546370bd7a",
+    "url": "/miStore-clone/index.html"
+  },
+  {
+    "revision": "a9401ba34a908fa8fee6",
+    "url": "/miStore-clone/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "12f7513eff59929b9ff3",
+    "url": "/miStore-clone/static/css/main.6d4a795b.chunk.css"
+  },
+  {
+    "revision": "a9401ba34a908fa8fee6",
+    "url": "/miStore-clone/static/js/2.73a29a9e.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/miStore-clone/static/js/2.73a29a9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f7513eff59929b9ff3",
+    "url": "/miStore-clone/static/js/main.366c21d1.chunk.js"
+  },
+  {
+    "revision": "1850f956b0ad06fe52d2",
+    "url": "/miStore-clone/static/js/runtime-main.ded4a024.js"
+  }
+]);
